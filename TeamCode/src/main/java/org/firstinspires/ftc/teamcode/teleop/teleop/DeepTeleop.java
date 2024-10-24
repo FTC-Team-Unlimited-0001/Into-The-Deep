dddbac822f4751707fa38c584f0a64df9c3c1f8f
Ksu977 <brosup490@gmail.com>
2024-10-24 17:23:58 -0400

    PID for angler
    The PID output is really week for some reason, try to fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/teleop/DeepTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/teleop/DeepTeleop.java
@@ -4,10 +4,12 @@ package org.firstinspires.ftc.teamcode.teleop.teleop;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
-import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.arcrobotics.ftclib.controller.PController;
+import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.teamcode.util.machine;
@@ -19,12 +21,13 @@ public class DeepTeleop extends LinearOpMode{
 
     private final FtcDashboard dashboard = FtcDashboard.getInstance();
 
+    public machine robot;
+
+
     @Override
     public void runOpMode() throws InterruptedException {
 
-
-        machine robot = new machine(hardwareMap);
-
+        robot = new machine(hardwareMap);
 
         //Telem stuff
         telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
@@ -40,9 +43,10 @@ public class DeepTeleop extends LinearOpMode{
 // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
         imu.initialize(parameters);
 
+
         waitForStart();
 
-        while (opModeIsActive()){
+        while (opModeIsActive()) {
 
             double y = -gamepad1.left_stick_y; // Remember, this is reversed!
             double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
@@ -52,26 +56,100 @@ public class DeepTeleop extends LinearOpMode{
             // This ensures all the powers maintain the same ratio, but only when
             // at least one is out of the range [-1, 1]
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-           double frontLeftPower = (y + x + rx) / denominator;
+            double frontLeftPower = (y + x + rx) / denominator;
             double backLeftPower = (y - x + rx) / denominator;
             double frontRightPower = (y - x - rx) / denominator;
             double backRightPower = (y + x - rx) / denominator;
 
-            if (gamepad1.left_bumper){
-                robot.frontLeft.setPower(frontLeftPower/4);
-                robot.backLeft.setPower(backLeftPower/4);
-                robot.frontRight.setPower(frontRightPower/4);
-                robot.backRight.setPower(backRightPower/4);
-            } else{
+            DcMotor.ZeroPowerBehavior brake = gamepad1.left_bumper ? DcMotor.ZeroPowerBehavior.BRAKE : DcMotor.ZeroPowerBehavior.FLOAT;
+            robot.frontLeft.setZeroPowerBehavior(brake);
+            robot.backRight.setZeroPowerBehavior(brake);
+            robot.backLeft.setZeroPowerBehavior(brake);
+            robot.backRight.setZeroPowerBehavior(brake);
+
+
+            if (gamepad1.left_bumper) {
+                robot.frontLeft.setPower(frontLeftPower / 4);
+                robot.backLeft.setPower(backLeftPower / 4);
+                robot.frontRight.setPower(frontRightPower / 4);
+                robot.backRight.setPower(backRightPower / 4);
+            } else {
                 robot.frontLeft.setPower(frontLeftPower);
                 robot.backLeft.setPower(backLeftPower);
                 robot.frontRight.setPower(frontRightPower);
                 robot.backRight.setPower(backRightPower);
             }
 
+            if (gamepad1.y) {
+                robot.anglerright.setPower(0.5);
+                robot.anglerleft.setPower(0.5);
+            } else {
+                robot.anglerright.setPower(0);
+                robot.anglerleft.setPower(0);
+            }
+            ;
 
+            if (gamepad1.b) {
+                robot.anglerright.setPower(-0.5);
+                robot.anglerleft.setPower(-0.5);
+            } else {
+                robot.anglerright.setPower(0);
+                robot.anglerleft.setPower(0);
+            }
+            ;
+
+            if (gamepad1.a) {
+                robot.spoolleft.setPower(1);
+                robot.spoolright.setPower(1);
+            } else {
+                robot.spoolright.setPower(0);
+                robot.spoolleft.setPower(0);
+            }
+
+
+            if (gamepad1.x) {
+                robot.spoolleft.setPower(-1);
+                robot.spoolright.setPower(-1);
+            } else {
+                robot.spoolright.setPower(0);
+                robot.spoolleft.setPower(0);
+            }
+
+            if (gamepad2.a) {
+                robot.armTargetPosition += 10;  // Increment target position (move up)
+            } else if (gamepad2.b) {
+                robot.armTargetPosition -= 10;  // Decrement target position (move down)
+            }
+
+          controlArmsWithPID();
+
+        }
+    }
+
+        // Method to control the arms using PID controller
+        private void controlArmsWithPID() {
+            // Get the current position of both arms (average)
+            double currentPosition = (robot.anglerleft.getCurrentPosition() + robot.anglerright.getCurrentPosition()) / 2.0;
+
+            // Calculate PID output
+            double armOutput = robot.armPIDController.calculate(currentPosition, robot.armTargetPosition);
+
+            // Apply the output to both motors
+            robot.anglerleft.setPower(armOutput);
+            robot.anglerright.setPower(armOutput);
+
+            // Telemetry to display information on the driver station
+            telemetry.addData("Arm Target", robot.armTargetPosition);
+            telemetry.addData("Arm Position", currentPosition);
+            telemetry.addData("PID Output", armOutput);
+            telemetry.addData("Left Arm Encoder", robot.anglerleft.getCurrentPosition());
+            telemetry.addData("Right Arm Encoder", robot.anglerright.getCurrentPosition());
+            telemetry.update();
 
         }
 
-    }
+
+
+
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/machine.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/machine.java
@@ -1,16 +1,35 @@
 package org.firstinspires.ftc.teamcode.util;
 
+import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.arcrobotics.ftclib.controller.PIDFController;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-
+@Config
 public class machine {
 
     public DcMotor frontRight;
     public DcMotor frontLeft;
     public DcMotor backRight;
     public DcMotor backLeft;
+
+    public DcMotor spoolleft;
+    public  DcMotor spoolright;
+
+    public DcMotor anglerright;
+    public DcMotor anglerleft;
+
+    public PIDController armPIDController;
+    public static double armTargetPosition;
+
+    // PID coefficients
+    public static double kP = 0.0000001;
+    public static double kI = 0.0;
+    public static double kD = 0.0000001;
+
+
 
 
     public machine(HardwareMap hwMap) {
@@ -28,6 +47,12 @@ public class machine {
         backRight = hardwareMap.get(DcMotor.class, "rightBack");
         backLeft = hardwareMap.get(DcMotor.class, "leftBack");
 
+        anglerright = hardwareMap.get(DcMotor.class, "anglerright");
+        anglerleft = hardwareMap.get(DcMotor.class, "anglerleft");
+
+        spoolleft = hardwareMap.get(DcMotor.class, "spoolleft");
+        spoolright = hardwareMap.get(DcMotor.class, "spoolright");
+
 
 
         //Set motor direction
@@ -35,8 +60,21 @@ public class machine {
         frontLeft.setDirection(DcMotor.Direction.REVERSE);
         backRight.setDirection(DcMotor.Direction.FORWARD);
         backLeft.setDirection(DcMotor.Direction.FORWARD);
+        anglerleft.setDirection(DcMotor.Direction.REVERSE);
 
 
+
+        anglerright.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        anglerright.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        anglerleft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        anglerleft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Initialize the PID controller for the arms
+        armPIDController = new PIDController(kP, kI, kD);
+
+        // Set an initial target position for the arms (e.g., 500 ticks)
+        armTargetPosition = 0;
 
     }
 }
