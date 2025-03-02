cfef1ebde51dd6c36ce97659f020e763d26bc91a
loknoovejikgvrjou <gsarin46@gmail.com>
2025-03-02 12:43:39 -0500

    changes to speed parameters, might work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -73,13 +73,15 @@ public class MecanumDrive {
         public double kA = 0.001;
 
         // path profile parameters (in inches)
-        public double maxWheelVel = 60.8;//50
-        public double minProfileAccel = -30;
-        public double maxProfileAccel = 50;
+        public double maxWheelVel = 65;
+        public double maxProfileAccel = 64;
+        public double minProfileAccel = -37;
 
         // turn profile parameters (in radians)
-        public double maxAngVel = Math.PI; // shared with path
-        public double maxAngAccel = Math.PI;
+        public double maxAngVel = 3.7;
+        public double maxAngAccel = 3.2;
+//        public double maxAngVel = Math.PI; // shared with path
+//        public double maxAngAccel = Math.PI;
 
         // path controller gains
         public double axialGain = 2;
